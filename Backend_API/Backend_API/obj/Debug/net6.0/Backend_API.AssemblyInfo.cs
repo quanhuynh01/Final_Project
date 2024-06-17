e445db482125dec6b4e0df960700d6dbb2ba7a16
Huỳnh Việt Quân <109968858+quanhuynh01@users.noreply.github.com>
2024-06-17 20:50:17 +0700

    quan1

--- Backend_API/Backend_API/obj/Debug/net6.0/Backend_API.AssemblyInfo.cs
+++ Backend_API/Backend_API/obj/Debug/net6.0/Backend_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f7c28596f4f3585970591ac331b6650f2c6033")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
