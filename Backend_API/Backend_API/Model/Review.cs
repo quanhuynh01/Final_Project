c71bfc9eec7fdb92e75da9153f21c5a53d51dcc6
Huỳnh Việt Quân <109968858+quanhuynh01@users.noreply.github.com>
2024-06-27 12:04:11 +0700

    quan chức năng review 27-06 12:04

---
+++ Backend_API/Backend_API/Model/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend_API.Model
+{
+    public class Review
+    {
+        public int Id { get; set; } 
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+        public string Name { get; set; }
+        public DateTime DateComment { get; set; }
+        public int Rating { get; set; }
+    }
+}
