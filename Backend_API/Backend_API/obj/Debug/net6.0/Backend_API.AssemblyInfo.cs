68c048d718792715e8911335e8ff2b3051417df9
TDuckHub <tdgamingytsv7@gmail.com>
2024-06-30 16:11:57 +0700

    b

--- Backend_API/Backend_API/obj/Debug/net6.0/Backend_API.AssemblyInfo.cs
+++ Backend_API/Backend_API/obj/Debug/net6.0/Backend_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f3a3d255d8e5ded2b0ad957173859fd0964733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36845f25ecd7cde4c698013db92a816c9972dcf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
